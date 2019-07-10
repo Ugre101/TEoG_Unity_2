0375d16038a9ad073c7494204c3dadee0e33d129
Ugre101 <44323564+Ugre101@users.noreply.github.com>
2019-07-10 23:24:39 +0200

    minor stuiff

---
+++ Assets/InventoryHandler.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class InventoryHandler : MonoBehaviour
+{
+    public GameObject ItemPrefab;
+    public playerMain player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private GameObject TheItem(Item item)
+    {
+        GameObject ItemObject = ItemPrefab;
+        TextMeshProUGUI[] texts = ItemObject.GetComponentsInChildren<TextMeshProUGUI>();
+        return ItemObject;
+    }
+}
