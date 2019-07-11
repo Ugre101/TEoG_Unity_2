f8c7e892fedcd11e4288252b05090930e2a621a8
Ugre101 <44323564+Ugre101@users.noreply.github.com>
2019-07-11 23:12:27 +0200

    Buildings
    Trigger to enter building,
    Sprite for townhall

---
+++ Assets/Scripts/Buildings/Buildings.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Buildings : MonoBehaviour
+{
+    public GameObject BuildingsMain;
+    public GameObject BuildingToEnter;
+   private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.tag == "Player")
+        {
+            BuildingsMain.SetActive(true);
+           foreach(Transform child in BuildingsMain.transform)
+            {
+                child.transform.gameObject.SetActive(false);
+            }
+            BuildingToEnter.SetActive(true);
+        }
+    }
+}
