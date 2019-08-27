1c696cacd5a8c067817630ac9bca94f03b6568b7
Ugre101 <44323564+Ugre101@users.noreply.github.com>
2019-08-27 22:41:21 +0200

    Auto change sprite first steps

---
+++ Assets/Scripts/Char/CharSprites.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Char sprites", menuName = "Char sprites")]
+public class CharSprites : ScriptableObject
+{
+    public Sprite humanMale;
+    public Sprite humanFemale;
+    public Sprite orcMale;
+    public Sprite elfMale;
+    public Sprite elfFemale;
+    public void OnEnable()
+    {
+        // Nice this works meaning I can use this to know what sprite to save!
+        Debug.Log(humanMale.name);
+    }
+}
