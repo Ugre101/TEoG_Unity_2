98fac1cca034c80c969f31bb950e2db0358eae43
Ugre101 <westin.gustav@gmail.com>
2019-09-14 08:50:09 +0200

    finaly right distance

--- Assets/Scripts/Menus/InventoryHoverText.cs
+++ Assets/Scripts/Menus/InventoryHoverText.cs
@@ -8,8 +8,10 @@ public class InventoryHoverText : MonoBehaviour
 {
     public GameObject hoverblock;
     public TextMeshProUGUI hovertext;
-    public float xDistance = 1f;
-    public float yDistance = 1f;
+    [Range(0f,0.5f)]
+    public float xDistance = 0.123f;
+    [Range(0f,0.5f)]
+    public float yDistance = 0.123f;
     private InventorySlot[] slots;
     private Item item;
     private DragInventory[] drag;
@@ -17,9 +19,9 @@ public class InventoryHoverText : MonoBehaviour
     private RectTransform hoverRect;
     public void OnEnable()
     {
-        slots = this.gameObject.GetComponentsInChildren<InventorySlot>();
-        drag = this.gameObject.GetComponentsInChildren<DragInventory>();
-        Parent = (RectTransform)this.gameObject.transform;
+        slots = GetComponentsInChildren<InventorySlot>();
+        drag = GetComponentsInChildren<DragInventory>();
+        Parent = (RectTransform)transform;
         hoverRect = (RectTransform)hoverblock.transform;
     }
     public void Hovering(GameObject hoverOver)
@@ -28,20 +30,21 @@ public class InventoryHoverText : MonoBehaviour
         GameObject InventorySlot = hoverOver.transform.parent.gameObject;
         RectTransform rt = (RectTransform)InventorySlot.transform;
         Vector3 vector3 = rt.position;
-        if (rt.position.x < Parent.rect.width / 2)
+        if (rt.position.x < Screen.width / 2)
         {
-            vector3.x += xDistance;
+            vector3.x += Screen.width * xDistance; // xDistance;
         }
         else
         {
-            vector3.x -= xDistance;
+            vector3.x -= Screen.width * xDistance;
         }
-        if (rt.position.y < Parent.rect.height / 2)
+        if (rt.position.y < Screen.height / 2)
         {
-            vector3.y += yDistance;
-        }else
+            vector3.y += Screen.height * yDistance;
+        }
+        else
         {
-            vector3.y -= yDistance;
+            vector3.y -= Screen.height * yDistance;
         }
         hoverRect.position = vector3;
         Item item = hoverOver.GetComponentInChildren<DragInventory>().item;
