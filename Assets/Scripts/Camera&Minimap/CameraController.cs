364ebcf56878a59980220aea79ba6447cf9d0228
Ugre101 <westin.gustav@gmail.com>
2019-10-01 14:33:48 +0200

    mobile zoom

--- Assets/Scripts/Camera&Minimap/CameraController.cs
+++ Assets/Scripts/Camera&Minimap/CameraController.cs
@@ -37,6 +37,32 @@ public class CameraController : MonoBehaviour
         _target.x = Mathf.Clamp(_target.x, _xMin, _xMax);
         _target.y = Mathf.Clamp(_target.y, _yMin, _yMax);
         // if Camera controll and check if bigger than tilemap
+
+        // Mobile zoom copied from unity learn
+        if (Input.touchCount == 2)
+        {
+            Touch touchZero = Input.GetTouch(0);
+            Touch touchOne = Input.GetTouch(1);
+
+            // Find the position in the previous frame of each touch.
+            Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
+            Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
+
+            // Find the magnitude of the vector (the distance) between the touches in each frame.
+            float prevTouchDeltaMag = (touchZeroPrevPos - touchOnePrevPos).magnitude;
+            float touchDeltaMag = (touchZero.position - touchOne.position).magnitude;
+
+            // Find the difference in the distances between each frame.
+            float deltaMagnitudeDiff = prevTouchDeltaMag - touchDeltaMag;
+
+            // If the camera is orthographic...
+
+            // ... change the orthographic size based on the change in distance between the touches.
+            _orthSize += deltaMagnitudeDiff * 0.1f;
+
+            // Make sure the orthographic size never drops below zero.
+            _orthSize = Mathf.Max(_orthSize, 0.1f);
+        }
         if (Input.GetKey(keyBindings.zoomInKey))
         {
             _orthSize -= 0.1f;
@@ -45,7 +71,7 @@ public class CameraController : MonoBehaviour
         {
             _orthSize += 0.1f;
         }
-        else
+        else 
         {
             _orthSize -= Input.GetAxis("Mouse ScrollWheel");
         }
