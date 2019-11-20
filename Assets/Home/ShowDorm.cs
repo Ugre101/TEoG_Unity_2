fe751656caf5d93aec149373018997c7e7f6468a
Ugre101 <westin.gustav@gmail.com>
2019-11-20 16:57:25 +0100

    dorm
    Moved dorm upgrades to dorm,
    Added empty text

--- Assets/Home/ShowDorm.cs
+++ Assets/Home/ShowDorm.cs
@@ -1,28 +1,63 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class ShowDorm : MonoBehaviour
 {
     public Dorm dorm;
     public GameObject container;
     public GameObject ServantListPrefab;
+    public GameObject upgrades, servantList;
+    public GameObject ifEmpty;
+    public Button upgradesBtn, back;
 
     // Start is called before the first frame update
+    private void Start()
+    {
+        upgradesBtn.onClick.AddListener(UpgradePart);
+        back.onClick.AddListener(ServantPart);
+    }
+
     public void OnEnable()
     {
+        ServantPart();
+    }
+
+    private void ServantPart()
+    {
+        upgrades.SetActive(false);
+        back.gameObject.SetActive(false);
+        upgradesBtn.gameObject.SetActive(true);
+        servantList.SetActive(true);
         ListServants();
+    }
+
+    private void UpgradePart()
+    {
+        upgrades.SetActive(true);
+        upgradesBtn.gameObject.SetActive(false);
+        servantList.SetActive(false);
+        back.gameObject.SetActive(true);
     }
 
     private void ListServants()
     {
-        foreach (Transform child in container.transform)
+        if (dorm.Servants.Count > 0)
         {
-            Destroy(child.gameObject);
+            ifEmpty.SetActive(false);
+            foreach (Transform child in container.transform)
+            {
+                Destroy(child.gameObject);
+            }
+            foreach (BasicChar Serv in dorm.Servants)
+            {
+                GameObject test = Instantiate(ServantListPrefab, container.transform);
+                ShowServant showServant = test.GetComponent<ShowServant>();
+                showServant.Init(Serv);
+            }
         }
-        foreach (BasicChar Serv in dorm.Servants)
+        else
         {
-            GameObject test = Instantiate(ServantListPrefab, container.transform);
-            ShowServant showServant = test.GetComponent<ShowServant>();
-            showServant.Init(Serv);
+            ifEmpty.SetActive(true);
         }
     }
 }
