8aea16794b4984f44fe8fa6f631f865b23cb7dc4
Ugre101 <westin.gustav@gmail.com>
2020-01-10 17:36:21 +0100

    Scenes

--- Assets/Scripts/AfterBattle/Sex Scenes/Scripts/Mouth/GiveCunnilingus.cs
+++ Assets/Scripts/AfterBattle/Sex Scenes/Scripts/Mouth/GiveCunnilingus.cs
@@ -1,8 +1,27 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+
 [CreateAssetMenu(fileName = "Give cunnilingus", menuName = ("Sex/Mouth/Give cunnilingus"))]
 public class GiveCunnilingus : SexScenes
 {
-   
+    public override string StartScene(PlayerMain player, BasicChar other)
+    {
+        Organs sexualOrgans = other.SexualOrgans;
+        Identity identity = other.Identity;
+        return $"Your foe lays on their back, chest heaving with exhaustion from the recent fight. You make your way up to {identity.FirstName}'s body and crouch between their legs, spreading them apart. In-between lies their {sexualOrgans.Vaginas[0].Race} pussy, engorged and dripping slightly, their clit twitching lewdly with each breath of your foe. You lick your lips in anticipation as you lower your head to your prize, spreading their lower lips with your fingers, and start eating them out.";
+    }
+
+    public override string ContinueScene(PlayerMain player, BasicChar other)
+    {
+        return $"Keeping them in place, you continue your tongue-lashing, electing soft moans from your opponent. Barely able to move, all {other.Identity.FirstName} can do is rest their hand on your head. You reach one hand to their clit and gently pinch it, sending a fresh wave of pleasure through their body.";
+    }
+
+    public override string OtherOrgasmed(PlayerMain player, BasicChar other)
+    {
+        return base.OtherOrgasmed(player, other);
+    }
+
+    public override string PlayerOrgasmed(PlayerMain player, BasicChar other)
+    {
+        return base.PlayerOrgasmed(player, other);
+    }
 }
--- Assets/Scripts/AfterBattle/Sex Scenes/Scripts/Vagina/GetCunnilingus.cs
+++ Assets/Scripts/AfterBattle/Sex Scenes/Scripts/Vagina/GetCunnilingus.cs
@@ -1,8 +1,25 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+
 [CreateAssetMenu(fileName = "Get cunnilingus", menuName = ("Sex/Vagina/Get cunnilingus"))]
 public class GetCunnilingus : SexScenes
 {
+    public override string StartScene(PlayerMain player, BasicChar other)
+    {
+        return base.StartScene(player, other);
+    }
 
+    public override string ContinueScene(PlayerMain player, BasicChar other)
+    {
+        return base.ContinueScene(player, other);
+    }
+
+    public override string OtherOrgasmed(PlayerMain player, BasicChar other)
+    {
+        return base.OtherOrgasmed(player, other);
+    }
+
+    public override string PlayerOrgasmed(PlayerMain player, BasicChar other)
+    {
+        return base.PlayerOrgasmed(player, other);
+    }
 }
