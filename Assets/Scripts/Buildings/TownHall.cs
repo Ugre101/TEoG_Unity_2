9fb186c18c7500bbf9a7dc6a58c0babd09d82277
Ugre101 <westin.gustav@gmail.com>
2020-03-09 19:04:02 +0100

    minor

--- Assets/Scripts/Buildings/TownHall.cs
+++ Assets/Scripts/Buildings/TownHall.cs
@@ -1,28 +1,32 @@
 ﻿using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class TownHall : Building
 {
     [SerializeField] private TextMeshProUGUI changeNameText = null;
-
     [SerializeField] private GameObject nameBox = null;
-
     [SerializeField] private TextMeshProUGUI textBox = null;
     [SerializeField] private GiveQuests giveQuests = new GiveQuests();
     [SerializeField] private GiveQuestRewards questRewards = new GiveQuestRewards();
-    [SerializeField] private Button btn = null;
     private string SetChangeNameBtnText { set => changeNameText.text = value; }
-    private string SetTextBox { set { textBox.text = value; } }
+
+    private void SetSetTextBox(string value) => textBox.text = value;
 
     // Start is called before the first frame update
     public void Start()
     {
-        giveQuests.QuestButtons.ForEach(q => q.Btn.onClick.AddListener(()
-            => Instantiate(giveQuests.QuestPanelPrefab, transform).Setup(q)));
-        questRewards.RewardButtons.ForEach(q => q.Btn.onClick.AddListener(()
-            =>
-        { textBox.text = QuestReward.GetReward(q.Quest); q.Btn.gameObject.SetActive(false); }));
+        foreach (QuestButton q in giveQuests.QuestButtons)
+        {
+            q.Btn.onClick.AddListener(() => Instantiate(giveQuests.QuestPanelPrefab, transform).Setup(q));
+        }
+        foreach (QuestRewardButton q in questRewards.RewardButtons)
+        {
+            q.Btn.onClick.AddListener(delegate
+            {
+                SetSetTextBox(QuestReward.GetReward(q.Quest));
+                q.Btn.gameObject.SetActive(false);
+            });
+        }
     }
 
     // TODO quest reward
@@ -39,6 +43,6 @@ public class TownHall : Building
         bool isActive = nameBox.activeSelf;
         nameBox.SetActive(!isActive);
         SetChangeNameBtnText = isActive ? "Change name" : "Back";
-        SetTextBox = "";
+        SetSetTextBox("");
     }
 }
