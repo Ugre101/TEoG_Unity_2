b46a182b421990bcc2165a72d15ef3b993b999db
Ugre101 <44323564+Ugre101@users.noreply.github.com>
2020-04-15 23:25:50 +0200

    vore

---
+++ Assets/ASinglePrey.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class ASinglePrey : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI title = null, desc = null;
+    [SerializeField] private Button backBtn = null;
+    [SerializeField] private Transform optionContainer = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
